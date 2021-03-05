9d816fc33a90833ebbacd4294b3bd7911b111836
sanyoni <ksyline1997@gmail.com>
2021-03-05 15:55:56 +0900

    solved: 10869-사칙연산

---
+++ sanyoni/10869.py
@@ -0,0 +1,12 @@
+# 문자열을 입력받고, 문자열을 공백(' ')으로 나눈 각 값을 a, b에 저장합니다.
+a, b = input().split()
+
+# 문자열을 숫자로 변환합니다.
+a = int(a)
+b = int(b)
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(int(a / b))
+print(a % b)
